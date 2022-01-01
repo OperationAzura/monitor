a4c2dd18baa8a798eea63f88ff5332cb854362b0
amazingfly <derekstich@gmail.com>
2022-01-01 01:01:07 +0000

    time lag fix and display fps

--- mClient.py
+++ mClient.py
@@ -1,34 +1,50 @@
 import cv2
 import screeninfo
+import time
 
 screen = screeninfo.get_monitors()[0]
 w, h = screen.width, screen.height
-"""
-#set
-centerX = int(width / 2)
-centerY = int(height / 2)
-radiusX = int(scale*centerX/100)
-radiusY = int(scale*centerY/100)
-minX = centerX - radiusX
-maxX = centerX + radiusX
-minY = centerY - radiusY
-maxY = centerY + radiusY
-"""
-cap = cv2.VideoCapture() 
-cap.open("http://192.168.1.38:8000/video")
+
+cap = cv2.VideoCapture("http://192.168.1.38:8000/video")
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+#cap.open("http://192.168.1.38:8000/video")
 
 wName = "Merrill Monitor"
 cv2.namedWindow(wName, cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty(wName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+org = (25, 75)
+orgFCount = (25, 175)
+fontScale = 3
+
+color = (0, 255, 0)
+
+thickness = 3
+
+fCount = 1
+fLimit = 30
+fps = 11111111111111111111
+fTime = time.time()
 while(True):
-    ret, frame = cap.read()
+    if fCount == 1:
+        fTime = time.time()
+    elif fCount == fLimit:
+        cap.release()
+        fCount = 1
+        fps = fLimit / (time.time() - fTime)
+        cap = cv2.VideoCapture('http://192.168.1.38:8000/video')
+        cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+    fCount += 1
+    #s = time.time()
+    _, frame = cap.read()
     frame = cv2.resize(frame, (w,h))
-    #cv2.namedWindow(wName, cv2.WND_PROP_FULLSCREEN)
-    #cv2.setWindowProperty(wName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
+    #print('read and resize: ', (time.time() - s))
+    #frame = cv2.putText(frame, 'fCount: '+str(fCount), orgFCount, font, fontScale, color, thickness, cv2.LINE_AA)
+    frame = cv2.putText(frame, 'FPS: '+str(fps), org, font, fontScale, color, thickness, cv2.LINE_AA)
     cv2.imshow(wName,frame)
-
+    #cap.release()
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
